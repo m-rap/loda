7bac950a553bcca2177e26975eb7f61898189419
Rian Prakoso <mohammad.rian.ap@gmail.com>
2020-07-13 11:55:56 +0700

    change printf to directly call java println

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,55 +2,50 @@
 #include <string>
 #include <dlfcn.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_mrap_loda_Loda_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+jmethodID getPrintStreamMetId(JNIEnv* env, jobject printStream) {
+    jclass clz = env->GetObjectClass(printStream);
+    return env->GetMethodID(clz, "println", "(Ljava/lang/String;)V");
 }
 
-void jniprintf(JNIEnv* env, jbyteArray jBuff, jlongArray jBuffTime, const char* fmt, ...) {
-    char* buff = (char*)env->GetByteArrayElements(jBuff, (jboolean*)0);
-
-    jlong* buffTime = env->GetLongArrayElements(jBuffTime, (jboolean*)0);
+void jniprintf(JNIEnv* env, jobject printStream, jmethodID metId, const char* fmt, ...) {
+    char buff[512];
 
     va_list args;
     va_start(args, fmt);
     sprintf(buff, fmt, args);
     va_end(args);
 
-    timeval t;
-    gettimeofday(&t, NULL);
-    buffTime[0] = t.tv_sec * 1000 + t.tv_usec / 1000;
+    jstring stringFromC = env->NewStringUTF(buff);
+    env->CallVoidMethod(printStream, metId, stringFromC);
 
-    env->ReleaseByteArrayElements(jBuff, (jbyte*)buff, 0);
-
-    env->ReleaseLongArrayElements(jBuffTime, buffTime, 0);
+    env->DeleteLocalRef(stringFromC);
 }
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_mrap_loda_Loda_loadLib(
         JNIEnv* env,
-        jobject /* this */, jstring jLibPath, jbyteArray jBuff, jlongArray jBuffTime) {
-    jniprintf(env, jBuff, jBuffTime, "inside loadLib\n");
+        jobject /* this */, jstring jLibPath, jobject printStream) {
+
+    jmethodID metId = getPrintStreamMetId(env, printStream);
+
+    jniprintf(env, printStream, metId, "inside loadLib\n");
     const char* libPath = env->GetStringUTFChars(jLibPath, (jboolean*)0);
     void* handle = dlopen(libPath, RTLD_LAZY);
     if (!handle) {
-        jniprintf(env, jBuff, jBuffTime, "dlopen error\n");
+        jniprintf(env, printStream, metId, "dlopen error\n");
         return env->NewStringUTF(dlerror());
     }
-    jniprintf(env, jBuff, jBuffTime, "dlopen success\n");
+    jniprintf(env, printStream, metId, "dlopen success\n");
     void (*execute)();
     execute = (void (*)())dlsym(handle, "execute");
     char* error = dlerror();
     if (error != NULL) {
         return env->NewStringUTF(error);
     }
-    jniprintf(env, jBuff, jBuffTime, "dlsym success\n");
+    jniprintf(env, printStream, metId, "dlsym success\n");
     execute();
     dlclose(handle);
     env->ReleaseStringUTFChars(jLibPath, libPath);
-    jniprintf(env, jBuff, jBuffTime, "handle closed\n");
+    jniprintf(env, printStream, metId, "handle closed\n");
     return env->NewStringUTF("success");
 }
--- app/src/main/java/com/mrap/loda/Loda.java
+++ app/src/main/java/com/mrap/loda/Loda.java
@@ -1,50 +1,20 @@
 package com.mrap.loda;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.io.PrintStream;
 
 public class Loda {
 
     String _libPath;
-    byte[] _buff = new byte[512];
-    long[] _buffTime = new long[] {0};
-    long _prevBuffTime = 0;
-    ScheduledExecutorService _es;
-
-    Runnable _printerRunner = new Runnable() {
-        @Override
-        public void run() {
-            if (_prevBuffTime != _buffTime[0]) {
-                int len = 0;
-                for (; len < 512 && _buff[len] != '\0'; len++);
-                System.out.println("from native: " + _buffTime[0] + " " + new String(_buff, 0, len));
-                _prevBuffTime = _buffTime[0];
-            }
-        }
-    };
 
     public Loda(String libPath) {
         _libPath = libPath;
-
-        _buff[0] = '\0';
-
-        _es = Executors.newSingleThreadScheduledExecutor();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-    private native String loadLib(String libPath, byte[] buff, long[] buffTime);
+    private native String loadLib(String libPath, PrintStream printStream);
 
     public String runLoadLib() {
         System.out.println("runLoadLib");
-        _es.scheduleAtFixedRate(_printerRunner, 0, 1, TimeUnit.MILLISECONDS);
-        String res = loadLib(_libPath, _buff, _buffTime);
-        _es.shutdown();
-        _es = Executors.newSingleThreadScheduledExecutor();
+        String res = loadLib(_libPath, System.out);
         return res;
     }
 
